362804125e7259f53d2e840f9e090d9096c50eea
Vincent Truchot <muskacirca@gmail.com>
2016-01-29 16:58:01 +0100

    Deploy

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var Schema = require('./data/schema');
 var graphQLHTTP = require('express-graphql');
 
 const app = express();
-app.use('/', graphQLHTTP({ schema: Schema, pretty: true, graphiql: true}));
+app.use('/graphql', graphQLHTTP({ schema: Schema, pretty: true, graphiql: true}));
 var graphQLIP = process.env.OPENSHIFT_NODEJS_PORT || 8080
+console.log("openshifht ip: " + process.env.OPENSHIFT_NODEJS_PORT)
 console.log("used ip: " + graphQLIP)
 app.listen(graphQLIP, function(err) {
     if (err)
     return console.error(err);
-console.log('GraphQL Server is now running on localhost:8080');
+console.log('GraphQL Server is now running on localhost:' + graphQLIP);
 });
