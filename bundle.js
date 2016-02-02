92836b3768cb7016050130efd1a876cc29e6f3ba
Vincent Truchot <vincent.truchot@transatel.com>
2016-02-02 15:47:46 +0100

    harmonize graphql version to 0.4.13

--- bundle.js
+++ bundle.js
@@ -63,7 +63,7 @@
 
 	app.listen(server_port, server_ip_address, function (err) {
 	    if (err) return console.error(err);
-	    console.log('GraphQL Server is now running on ' + server_ip_address + ':' + server_port);
+	    console.log('GraphQL Server is now running on ' + server_ip_address + ':' + server_port + '/graphql');
 	});
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
 
